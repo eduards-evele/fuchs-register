18ce2fbeeef5bf83c154b0e0fbdac45993784d36
Eduards Evele <ee230302@gmail.com>
2026-02-28 00:17:08 +0000

    Fixed locales

--- fuchs_register/urls.py
+++ fuchs_register/urls.py
@@ -18,7 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("register/", include("register.urls")),
     path('i18n/', include('django.conf.urls.i18n')),
 ]
+
+urlpatterns += i18n_patterns(
+    path('admin/', admin.site.urls),
+    path('', include('register.urls')),
+)
