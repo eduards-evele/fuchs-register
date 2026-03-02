e45f4b096ae355dcc3e5a928fd88e1a7192ec4a4
Eduards Evele <ee230302@gmail.com>
2026-03-02 14:24:05 +0000

    Added missing import

--- fuchs_register/urls.py
+++ fuchs_register/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
     path('i18n/', include('django.conf.urls.i18n')),
